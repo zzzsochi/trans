fc3a02ed585bfb4d8ad404bceeebefae5496ebd4
Alexander Zelenyak <zzz.sochi@gmail.com>
2015-11-04 15:20:37 +0300

    Extend supported python versions and some pep8 fixes

--- setup.py
+++ setup.py
@@ -9,25 +9,27 @@ long_description = open('README.rst', 'r').read()
 description = 'National characters transcription module.'
 
 setup(
-        name='trans',
-        version=trans.__version__,
-        description=description,
-        long_description=long_description,
-        author='Zelenyak Aleksandr aka ZZZ',
-        author_email='zzz.sochi@gmail.com',
-        url='https://github.com/zzzsochi/trans',
-        license='BSD',
-        platforms='any',
+    name='trans',
+    version=trans.__version__,
+    description=description,
+    long_description=long_description,
+    author='Zelenyak Aleksander aka ZZZ',
+    author_email='zzz.sochi@gmail.com',
+    url='https://github.com/zzzsochi/trans',
+    license='BSD',
+    platforms='any',
 
-        classifiers=[
-                'Development Status :: 5 - Production/Stable',
-                'Intended Audience :: Developers',
-                'Operating System :: OS Independent',
-                'Programming Language :: Python',
-                'Programming Language :: Python :: 2.6',
-                'Programming Language :: Python :: 2.7',
-                'Programming Language :: Python :: 3.3',
-        ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
 
-        py_modules=['trans'],
+    py_modules=['trans'],
 )
